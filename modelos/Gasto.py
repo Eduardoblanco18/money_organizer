a94828e825b3b140c8a2cd91d6021df689626258
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2026-07-03 21:48:30 -0300

    Modificando as classes

--- modelos/Gasto.py
+++ modelos/Gasto.py
@@ -3,3 +3,31 @@ class Gasto:
         self._descricao = descricao
         self._categoria = categoria
         self._valor = valor
+
+    @property
+    def descricao(self):
+        return self._descricao
+
+    @property
+    def categoria(self):
+        return self._categoria
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    def editar_descricao(self, descricao_nova):
+        self._descricao = descricao_nova
+
+    def editar_categoria(self, categoria_nova):
+        self._categoria = categoria_nova
+
+    def editar_valor(self, valor_novo):
+        self._Valor = valor_novo
+
+    def para_dict(self):
+        return {
+            "Descrição": self.descricao,
+            "Categoria": self.categoria,
+            "Valor": self.valor
+        }
--- modelos/Orcamento.py
+++ modelos/Orcamento.py
@@ -1,3 +1,4 @@
+import os
 import csv
 from modelos.Gasto import Gasto
 
@@ -18,6 +19,13 @@ class Orcamento:
     def investimentos(self):
         return self._salario*0.2
     
+    @property
+    def gastos(self):
+        return self._gastos
+    
+    def limpar_tela(self):
+        os.system("cls")
+    
     def adicionar_gasto(self, descricao, categoria, valor):
         gasto = Gasto(descricao, categoria, valor)
 
@@ -27,9 +35,9 @@ class Orcamento:
         del(self._gastos[posicao])
     
     def calcular_gasto(self, categoria):
-        return sum(gasto._valor 
+        return sum(gasto.valor 
                 for gasto in self._gastos
-                if gasto._categoria == categoria
+                if gasto.categoria == categoria
                 )
     
     def salvar_gastos(self):
@@ -39,7 +47,7 @@ class Orcamento:
             escritor.writeheader()
 
             for gasto in self._gastos:
-                escritor.writerow(gasto)
+                escritor.writerow(gasto.para_dict())
 
     def carregar_historico(self):
         with open("gastos.csv","r", encoding="utf-8") as arquivo:
@@ -52,4 +60,96 @@ class Orcamento:
                     float(conteudo["Valor"])
                 )
                 self._gastos.append(gasto)
+
+    def menu_edicao(self):
+        self.limpar_tela()
+        print(f"{"Nº":<3} {"Descrição":<15}{"Categoria":<20}{"Valor"}")
+        print("-"*50)
+        for indice, gasto in enumerate(self._gastos):
+            print(f"{indice + 1:<3} {gasto.descricao:<15}{gasto.categoria:<20}{gasto.valor}")
+
+        posicao_valida = False
+        while not posicao_valida:
+            try:
+                posicao = int(input("Escolha um item que queira editar\n>")) - 1
+                if 0 <= posicao < len(self._gastos):
+                    posicao_valida = True
+                else:
+                    print("Valor incompatível!")
+            except ValueError:
+                print("Valor incompatível!")
+        self.limpar_tela()
+        print("""
+O que deseja editar?
+1- Trocar Nome
+2- Trocar Categoria
+3- Trocar Valor
+4- Excluir
+""")
+        
+        opcao_acessivel = False
+        while not opcao_acessivel:
+            try:
+                opcao_escolhida = int(input(">"))
+                opcao_acessivel = True
+                match opcao_escolhida:
+                    case 1:
+                        self.editar_gasto("Descrição", posicao)
+                    case 2:
+                        self.editar_gasto("Categoria", posicao)
+                    case 3:
+                        self.editar_gasto("Valor", posicao)
+                    case 4:
+                        self.excluir_gasto(posicao)
+                    case _:
+                        print("Opção Inválida")
+                        opcao_acessivel = False
+            except ValueError:
+                print("Opção Inválida")
+
+    def editar_gasto(self, detalhe, posicao):
+        self.limpar_tela()
+        gasto = self._gastos[posicao]
+        edicao_fazivel = False
+        while not edicao_fazivel:
+            try:
+                edicao_fazivel = True
+                if detalhe == "Descrição":
+                    edicao = input("Coloque o novo nome\n>")
+                    gasto.editar_descricao(edicao)
+                elif detalhe == "Categoria":
+                    edicao = self.escolher_categoria()
+                    gasto.editar_categoria(edicao)
+                else:
+                    edicao = float(input("Coloque o novo valor\n>"))
+                    gasto.editar_valor(edicao)
+            except ValueError:
+                edicao_fazivel = False
+                print("Não é possível realizar a edição")
+
+    def escolher_categoria(self):
+        escolha_certa = False
+        while not escolha_certa:
+            print(
+"""Qual categoria ele se encaixa?
+1- Necessidade
+2- Lazer
+3- Investimentos""")
+            try:
+                opcao = int(input(">"))
+                match opcao:
+                    case 1:
+                        categoria = "Necessidade"
+                        escolha_certa = True
+                    case 2:
+                        categoria = "Lazer"
+                        escolha_certa = True
+                    case 3:
+                        categoria = "Investimentos"
+                        escolha_certa = True
+                    case _:
+                        print("Não existe essa opção")
+            except ValueError:
+                print("Não existe essa opção")
+        return categoria
     
